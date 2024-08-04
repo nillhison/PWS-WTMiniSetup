0df6adce0c6272440d7d2d1070cb5463c5adf86a
nillhison <nillhison@outlook.com>
2024-08-04 16:31:44 -0300

    Starting to develop a setup

---
+++ setup.ps1
@@ -0,0 +1,58 @@
+# Run this file: powershell -ExecutionPolicy Bypass .\setup.ps1
+
+# View the profile files and its scopes: $PROFILE | Select-Object *
+
+# Set the execution policy to RemoteSigned in order to perform the setup
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+class InstallRequired {
+
+  [void] Modules([string[]]$modules) {
+		Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+		foreach($module in $modules) {
+			Install-Module -Name $module -Force -Scope CurrentUser
+		}
+  }
+  
+  [void] Applications([string[]]$applications) {
+    foreach($id in $frameworks.Keys) {
+			if (!(Get-Command $id -ErrorAction SilentlyContinue)) {
+				winget install --id $applications[$id] -e --source winget -s user
+			}
+    }
+  }
+  
+}
+
+$install = [InstallRequired]::new()
+
+$modules = @(
+	"Terminal-Icons",
+	"PSReadLine",
+	"posh-git"
+)
+
+$applications = @ {
+  "git" = "Git.Git",
+  "gh" = "GitHub.cli",
+  "code" = "Microsoft.VisualStudioCode",
+  "oh-my-posh" = "JanDeDobbeleer.OhMyPosh"
+}
+
+$install.Modules($modules)
+$install.Applications($applications)
+
+$env:Path += ";APPDATA\Local\Programs\oh-my-posh\bin"
+oh-my-posh font install meslo
+
+# Create a "Current User, Current Host" profile if it does not exist
+if (!(Test-Path -Path $PROFILE)) {
+  New-Item -ItemType File -Path $PROFILE -Force
+}
+
+# Write the profile script into $PROFILE
+Copy-Item profile.ps1 -Destination $PROFILE
+
+# Reloading the profile
+. $PROFILE
+
