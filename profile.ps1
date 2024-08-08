9d40a43b414e948924efaf74d079b62187391012
nillhison <nillhison@outlook.com>
2024-08-08 19:59:16 -0300

    Reviewing code and adding more comments

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,15 @@
-# Setting themes
+# Set the theme
+# Use Takuya theme
 oh-my-posh init pwsh --config 'APPDATA\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
+
+# Or use Probua.minimal (uncomment the line bellow)
 # oh-my-posh init pwsh --config 'APPDATA\Local\Programs\oh-my-posh\themes\probua.minimal.omp.json' | Invoke-Expression
 
+# Modules imports
 Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
 
-# Setting predictions viewer options
+# Set predictions viewer options
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
--- setup.ps1
+++ setup.ps1
@@ -5,28 +5,35 @@
 # Set the execution policy to RemoteSigned in order to perform the setup
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+# Create this class to interact with the installer application method in InstallRequired
 class AppInfo {
     
     $info = [PSCustomObject]@{}
 
     AppInfo([string]$appId, [string]$appExe, [string]$appName, [string]$appScope) {
-        $info.Id = $appId
-        $info.Exe = $appExe
-        $info.Name = $appName
-        $info.Scope = $appScope
+        $this.info.Id = $appId
+        $this.info.Exe = $appExe
+        $this.info.Name = $appName
+        $this.info.Scope = $appScope
     }
 
     [PSCustomObject] Info() {
-        return $info
+        return $this.info
     }
     
 }
 
+# Create the core class for this setup
 class InstallRequired {
 
     [void] Modules([string[]]$mdls) {
+        
+        # Trust the repository's authors
         Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        
         foreach($mdl in $mdlss) {
+            
+            # Install the module, if not yet installed
             if (-not (Get-Module -ListAvailable -Name $mdl)) {
                 try {
                     Install-Module -Name $mdl -Force -Scope CurrentUser
@@ -36,75 +43,102 @@ class InstallRequired {
             } else {
                 Write-Host "Module $mdl is already installed"
             }
+            
         }
+        
     }
 
     [void] Applications([AppInfo[]]$apps) {
+        
         foreach($app in $apps) {
+            
+            # Look up for the application in the system
+            # Install it, if not yet installed
             if (-not (Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $app.Info().Name })) {
                 try {
-                    winget install --id $app.Info().Id -e --source winget -scope $app.Info().Scope
+                    winget install --id $app.Info().Id -e --source winget --scope $app.Info().Scope
                 } catch {
                     Write-Error "Failed to install application $app.Info().Name. Error: $_"
                 }
-                $this.SetAppEnv($app.Info().Exe, $app.Info().Scope)
             } else {
                 Write-Host "Application $app.Info().Name is already installed"
-                if() {
-                    $this.SetAppEnv($app.Info().Exe, $app.Info().Scope)
-                }
             }
+            
+            # Update the PATH setting an environment variable to the .exe file
+            $this.SetAppEnv($app.Info().Exe, $app.Info().Scope)
         }
+        
     }
     
     [void] SetAppEnv([string]$exe, [string]$scope) {
         
-        $exePath
-        $exeDirectory = $exePath.DirectoryName
+        # Create a variable that holds the directory for the .exe file, regarding its scope
+        # Create a variable to adapt the scope nomenclature from winget to .NET
         
         switch($scope) {
             'machine' {
-                $exePath = Get-ChildItem -Path "C:\Program Files", "C:\Program Files (x86)" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq $exe }
-                [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$exeDirectory", [System.EnvironmentVariableTarget]::Machine)
+                $exePath  = Get-ChildItem -Path "C:\Program Files", "C:\Program Files (x86)" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq $exe }
+                $exeDirectory = $exePath.DirectoryName
+                $dotnetScope = 'Machine'
             }
             'user' {
-                $exePath = Get-ChildItem -Path "APPDATA\Local\Programs" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq $exe }
-                [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$exeDirectory", [System.EnvironmentVariableTarget]::CurrentUser)
+                $exePath  = Get-ChildItem -Path "APPDATA\Local\Programs" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq $exe }
+                $exeDirectory = $exePath.DirectoryName
+                $dotnetScope = 'CurrentUser'
             }
+        }
+        
+        # If the application was already installed, check whether it's in the PATH
+        # Also applied if this instalation itself has just added the .exe to the PATH
+        if(-not (Test-Path -Path $exeDirectory)) {
+            # Use .NET context to append the .exe directory to the PATH
+            [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$exeDirectory", [System.EnvironmentVariableTarget]::$dotnetScope)
         }
     }
 
 }
 
+# Create the installer variable
 $install = [InstallRequired]::new()
 
+# Create the variable that wraps the modules informations (in this case, only their names)
 $modules = @(
     "Terminal-Icons",
     "PSReadLine",
     "posh-git"
 )
 
+# Create the variable that wraps the applications informations
 $applications = @(
-    [AppInfo]::new('git', 'Git.Git', 'Git', 'machine'),
-    [AppInfo]::new('gh', 'GitHub.cli', 'GitHub CLI', 'machine'),
-    [AppInfo]::new('code', 'Microsoft.VisualStudioCode', 'Microsoft Visual Studio Code', 'user'),
-    [AppInfo]::new('oh-my-posh', 'JanDeDobbeleer.OhMyPosh', 'Oh My Posh', 'user')
+    [AppInfo]::new('Git.Git', 'git', 'Git', 'machine'),
+    [AppInfo]::new('GitHub.cli', 'gh', 'GitHub CLI', 'machine'),
+    [AppInfo]::new('Microsoft.VisualStudioCode', 'code' 'Microsoft Visual Studio Code', 'user'),
+    [AppInfo]::new('JanDeDobbeleer.OhMyPosh', 'oh-my-posh', 'Oh My Posh', 'user')
 )
 
-
+# Finally...
+# Pass the data items variables to the installer and proceed the instalation
 $install.Modules($modules)
 $install.Applications($applications)
 
-$env:Path += ";APPDATA\Local\Programs\oh-my-posh\bin"
+# Install the additional items
 oh-my-posh font install meslo
 
 # Create a "Current User, Current Host" profile if it does not exist
-if (!(Test-Path -Path $PROFILE)) {
+if(-not (Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
 }
 
 # Write the profile script into $PROFILE
-Copy-Item profile.ps1 -Destination $PROFILE
-
-# Reloading the profile
-. $PROFILE
+# Although the template is located in this directory, check this before proceeding
+if(Test-Path -Path "profile.ps1") {
+    
+    Copy-Item profile.ps1 -Destination $PROFILE
+    
+    # Finally...
+    # Reload the profile
+    . $PROFILE
+    
+} else {
+    Write-Host "Profile template not found!"
+}
